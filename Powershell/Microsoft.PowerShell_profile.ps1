abd13aa2bd628aee081725c0a1d5f6d9dd9f5d3b
SahilMadan <hillatio@gmail.com>
2015-11-06 17:19:52 +1030

    Powershell files added.

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+# Set Alias for Programs
+New-Item alias:subl -value 'C:\Program Files\Sublime Text 3\subl.exe'
+New-Item alias:Invoke-VSTools -value 'C:\Users\Sahil\Documents\WindowsPowerShell\Invoke-VSTools.ps1'
+New-Item alias:git -value 'C:\Program Files (x86)\Git\cmd\git.exe'
